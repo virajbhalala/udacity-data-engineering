a87b66861bd557ffc453de02e13e53b6b583507e
Bhalala <virajbhalala@gmail.com>
2020-09-12 11:03:15 -0400

    proj submitted

--- Data_Warehouse/create_tables.py
+++ Data_Warehouse/create_tables.py
@@ -4,12 +4,15 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """This function executes drop queries from sql_queries.py script if table already exist."""
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """This function executes creating fact, dimension, and staging table queries from sql_queries.py"""
+
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
@@ -18,13 +21,11 @@ def create_tables(cur, conn):
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
-
+    print(*config['CLUSTER'].values())
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
-
     drop_tables(cur, conn)
     create_tables(cur, conn)
-
     conn.close()
 
 
--- Data_Warehouse/etl.py
+++ Data_Warehouse/etl.py
@@ -4,12 +4,14 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """Load data from json files in S3 bucket to staging tables on Redshift using queries in sql_queries.py."""
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """This function exectues queries to transform data from staging tables and insert into analytic tables on Redshift as defined in sql_queries.py."""
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
